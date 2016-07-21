35f69cd814831105bbdb7652f09ca36ea3747c47
ZhangFei <15634987239@126.com>
2016-07-21 14:30:19 +0800

    添加推送

---
+++ JPush-iOS-SDK-2.1.7/demo/PushTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  PushTest
+//
+//  Created by LiDong on 12-8-15.
+//  Copyright (c) 2012年 HXHG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+static NSString *appKey = @"c9f55828e20e2ea705664375";
+static NSString *channel = @"Publish channel";
+static BOOL isProduction = FALSE;
+
+@interface AppDelegate : NSObject<UIApplicationDelegate> {
+  UILabel *_infoLabel;
+  UILabel *_tokenLabel;
+  UILabel *_udidLabel;
+}
+@property(strong, nonatomic) IBOutlet UITabBarController *rootController;
+@property(retain, nonatomic) UIWindow *window;
+
+@end
